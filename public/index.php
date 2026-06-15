0f5d20db5290600b37549e81d9144982c97d85f5
ennafiaasma-art <ennafiaasma@gmail.com>
2026-06-15 15:32:17 +0100

    Configure ton routeur pour qu'il sache faire la différence entre une route classique (ex: /admin/reports) et une route API (ex: /api/v1/batches).

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php 
+require_once __DIR__ ."/../src/controller/Api/ApiDashboardController.php";
+require_once __DIR__ ."/../src/controller/web/AdminController.php";
 $url=$_GET['url'] ?? "dashboard" ;
 switch($url){
     case 'dashboard':
